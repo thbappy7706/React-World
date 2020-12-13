eeba4f670a6779f10b78563ee2da6d751da22ec1
Tanvir Hossen Bappy <bappy.mccltd@gmail.com>
2020-12-13 15:10:43 +0600

    components in react

---
+++ src/React Prac/Components in React/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Body()
+{
+    return <ol>
+        <li>Welcome to MCC Ltd</li>
+        <li>Humayun Road Mohammadpur</li>
+    </ol>
+}
+
+export default Body;
---
+++ src/React Prac/Components in React/Heading.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Heading()
+{
+    return <h1>Hi Tanvir Hossen Bappy</h1>
+}
+export default Heading;
