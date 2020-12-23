86d6ec3aec113da2bfd692e8869c2dca9a5d6c6d
Tanvir Hossen Bappy <bappy.mccltd@gmail.com>
2020-12-23 14:53:55 +0600

    Base files Backup

---
+++ src/Base/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+           Hi, Tanvir Hossen Bappy
+        </h2>
+        <p>Welcome to React</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Base/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Heading from './Heading';
+import Body from './Body';
+
+function App(){
+    return(
+        <>
+        <Heading/>
+        <Body/>
+        </>
+    );
+}
+export default App;
